3d0e2ec744a6a70504f42d97802ed6249e6b91cc
KakishimaItsuki <porutagaisut@yahoo.co.jp>
2014-04-28 17:02:47 +0900

    miniCourse
    a

---
+++ Assets/CarController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class CarController : MonoBehaviour {
+
+	public float Accleration = 10f;
+	public float HandleSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate(){
+		float v = Input.GetAxisRaw ("Vertical");
+		float h = Input.GetAxisRaw ("Horizontal");
+
+		if (rigidbody.detectCollisions){
+			// rotation
+			Vector3 rotateDirection = transform.TransformDirection(Vector3.up * h);
+			rigidbody.angularVelocity = rotateDirection * HandleSpeed;
+
+			// accleration
+			Vector3 forceDirection = transform.TransformDirection(Vector3.forward * v);
+			rigidbody.AddForce (forceDirection * Accleration);
+		}
+	}
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello!");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.rigidbody.AddForce (new Vector3 (10, 0, 0));
+	}
+}
